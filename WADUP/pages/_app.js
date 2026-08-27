56721a54d5c6c85d926cfbe14f9c4a50ef689411
whb21burton <whb21burton@gmail.com>
2026-08-27 17:03:07 -0400

    add TM affiliate tracking pixel

--- WADUP/pages/_app.js
+++ WADUP/pages/_app.js
@@ -11,6 +11,12 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-title" content="WadUp" />
         <link rel="apple-touch-icon" href="/icon-192.png" />
         <meta name="theme-color" content="#050d1a" />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7691820-e643-49f0-b60a-b1d1cb2a49c41.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
