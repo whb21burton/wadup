53355d2d18002e02e5b2fe4066ee80364a323ff3
whb21burton <whb21burton@gmail.com>
2026-08-27 12:35:49 -0400

    PWA manifest and icons

--- WADUP/pages/_app.js
+++ WADUP/pages/_app.js
@@ -1,5 +1,18 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="WadUp" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="theme-color" content="#050d1a" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
